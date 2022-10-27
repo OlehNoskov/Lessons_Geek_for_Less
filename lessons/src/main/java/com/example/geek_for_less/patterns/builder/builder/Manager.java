7f1f370db8910edf79a392e775b644fad1780bc0
OlehNoskov <olega6000@gmail.com>
2022-10-27 16:38:13 +0300

    testing

--- lessons/src/main/java/com/example/geek_for_less/patterns/builder/builder/Manager.java
+++ lessons/src/main/java/com/example/geek_for_less/patterns/builder/builder/Manager.java
@@ -4,16 +4,16 @@ public class Manager {
 
     public void constructorAutoBuild(Builder builder){
         builder.setId(1);
-        builder.setType("Constructor Auto Build");
+        builder.setType("Constructor Auto Build!");
     }
 
     public void constructorManualBuild(Builder builder){
         builder.setId(2);
-        builder.setType("Constructor Manual Build");
+        builder.setType("Constructor Manual Build!");
     }
 
     public void constructorOtherBuild(Builder builder){
         builder.setId(3);
-        builder.setType("Constructor Other Build");
+        builder.setType("Constructor Other Build!");
     }
 }
