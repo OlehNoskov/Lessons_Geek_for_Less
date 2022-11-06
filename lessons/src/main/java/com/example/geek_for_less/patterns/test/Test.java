92216581b3c4a5b5e2e466b574a74af62bb64110
OlehNoskov <olega6000@gmail.com>
2022-11-06 17:39:30 +0200

    commit without git add

--- lessons/src/main/java/com/example/geek_for_less/patterns/test/Test.java
+++ lessons/src/main/java/com/example/geek_for_less/patterns/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     private static String text2 = "test2";
     private static String text3 = "test3";
     private static String text4 = "test4";
+    private static String text5 = "test5";
     public static void main(String[] args) {
         System.out.println(text);
     }
