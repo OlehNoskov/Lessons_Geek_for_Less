460be8dc27f6705e1a38155e65b454b69cad2fd3
OlehNoskov <olega6000@gmail.com>
2022-11-06 18:24:40 +0200

    create show method

--- lessons/src/main/java/com/example/geek_for_less/patterns/test/Test.java
+++ lessons/src/main/java/com/example/geek_for_less/patterns/test/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("TEXT!!!");
     }
+
+    public static void show(){
+        System.out.println("TEXT 2 !!!");
+    }
 }
