50b5e28144bb33fc441beaff2e82cd57e8805cb2
OlehNoskov <olega6000@gmail.com>
2022-11-06 17:32:57 +0200

    change class Test

--- lessons/src/main/java/com/example/geek_for_less/patterns/test/Test.java
+++ lessons/src/main/java/com/example/geek_for_less/patterns/test/Test.java
@@ -1,7 +1,11 @@
 package com.example.geek_for_less.patterns.test;
 
 public class Test {
+    private static String text = "test";
+    private static String text2 = "test2";
+    private static String text3 = "test3";
+    private static String text4 = "test4";
     public static void main(String[] args) {
-        
+        System.out.println(text);
     }
 }
