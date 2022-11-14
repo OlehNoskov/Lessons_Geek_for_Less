22585119ce56e0e9045fa8ead95612fcf3e352e3
OlehNoskov <olega6000@gmail.com>
2022-11-14 22:51:51 +0200

    Create dto Person and Address

---
+++ lessons/src/main/java/com/example/geek_for_less/dto/Address.java
@@ -0,0 +1,67 @@
+package com.example.geek_for_less.dto;
+
+import java.util.Objects;
+
+public class Address {
+
+    private String city;
+    private String street;
+    private int buildingNumber;
+
+    public Address() {
+    }
+
+    public Address(String city, String street, int buildingNumber) {
+        this.city = city;
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(int buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return buildingNumber == address.buildingNumber &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, buildingNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", buildingNumber=" + buildingNumber +
+                '}';
+    }
+}
---
+++ lessons/src/main/java/com/example/geek_for_less/dto/Person.java
@@ -0,0 +1,65 @@
+package com.example.geek_for_less.dto;
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private Address address;
+
+    public Person() {
+    }
+
+    public Person(String name, String surname, Address address) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
