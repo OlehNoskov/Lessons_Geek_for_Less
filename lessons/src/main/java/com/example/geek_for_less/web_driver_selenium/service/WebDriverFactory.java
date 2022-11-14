003827e00403a89627b84fdc6ed9a316f51b320d
OlehNoskov <olega6000@gmail.com>
2022-11-14 17:10:06 +0200

    Create WebDriverFactory and it implementaion

---
+++ lessons/src/main/java/com/example/geek_for_less/web_driver_selenium/service/WebDriverFactory.java
@@ -0,0 +1,7 @@
+package com.example.geek_for_less.web_driver_selenium.service;
+
+import org.openqa.selenium.WebDriver;
+
+public interface WebDriverFactory {
+    WebDriver create();
+}
---
+++ lessons/src/main/java/com/example/geek_for_less/web_driver_selenium/service/WebDriverFactoryChromeDriver.java
@@ -0,0 +1,34 @@
+package com.example.geek_for_less.web_driver_selenium.service;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WebDriverFactoryChromeDriver implements WebDriverFactory {
+
+    // Set System.setProperty for chrome driver and specify path for chromedriver.exe file
+    static {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe ");
+    }
+
+    @Override
+    public WebDriver create() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        // First way
+        // for proxy Chrome driver object example addArguments
+        chromeOptions.addArguments("--proxy-server-<username>:<password>@<host>:<port>");
+
+        // Second way
+        //Map includes preferences settings for gadgets or desktop version
+        Map<String, Object> pref = new HashMap<>();
+        pref.put("deviceName", "Iphone X");
+        // SetExperimentalOption is working only for Google Chrome!!!
+        chromeOptions.setExperimentalOption("mobileEmulation", pref);
+
+        return new ChromeDriver(chromeOptions);
+    }
+}
