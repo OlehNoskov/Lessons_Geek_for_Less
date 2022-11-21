5f9846b4435beb775efc98255400baa8ccd038bf
OlehNoskov <olega6000@gmail.com>
2022-11-21 16:27:40 +0200

    Testing merge with conflicts

--- lessons/src/main/java/com/example/geek_for_less/test/Test.java
+++ lessons/src/main/java/com/example/geek_for_less/test/Test.java
@@ -3,6 +3,9 @@ package com.example.geek_for_less.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Test!");
+        System.out.println("Test 1");
+        System.out.println("Test 2");
+        System.out.println("Test 3");
+        System.out.println("Test 4");
     }
 }
