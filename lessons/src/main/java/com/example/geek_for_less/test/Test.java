5f7d9e0881fc7a0ad1a46a942291675113818887
OlehNoskov <olega6000@gmail.com>
2022-12-06 16:41:58 +0200

    Update project

--- lessons/src/main/java/com/example/geek_for_less/test/Test.java
+++ lessons/src/main/java/com/example/geek_for_less/test/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("Test 3");
         System.out.println("Test 4");
         System.out.println("Test 5");
+        System.out.println("Test 6");
     }
 }
